4cd48ad568d6215f25160249b7a0aeee836d564f
Volodymyr Myrza <vmyrza@magecore.com>
2016-12-10 14:06:14 +0200

    Making auto-update lib.  - add closure

--- t.js
+++ t.js
@@ -5,12 +5,7 @@
 
 
 //JS to update main JS
-var VERSION = '0.0.2a';
-
-
-function main() {
-    flash('VERSION ' + VERSION + ' Installed.');
-}
-
-
-main();
+!function(tasker){
+    var VERSION = '0.0.2a';
+    tasker.flash('VERSION ' + VERSION + ' Installed.');
+}(this);
