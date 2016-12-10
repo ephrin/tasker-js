5a102a8cd2f167465ed88a01ea83f47b8ec5e211
Volodymyr Myrza <vmyrza@magecore.com>
2016-12-10 13:58:25 +0200

    Making auto-update lib.  - sdk check

--- t.js
+++ t.js
@@ -1,11 +1,10 @@
+//JS to update main JS
 var VERSION = '0.0.2a';
 
-if (global('sdk') > 0) {
-    main();
-} else {
-    flash('Error. No SKD detected. ('+VERSION+')');
-}
 
 function main() {
     flash('VERSION ' + VERSION + ' Installed.');
 }
+
+
+main();
